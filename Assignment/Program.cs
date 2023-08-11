206465e7c21cb6a16b2af532a90974f2f595d325
anshu <anshu@ANSH>
2023-08-11 14:15:19 +0530

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the elements of the array separated by spaces: ");
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.Write("Enter the value of k: ");
+            int k = int.Parse(Console.ReadLine());
+
+            Solution solution = new();
+            IList<int> result = solution.MaxFrequency(nums, k);
+
+            Console.WriteLine("Top k frequent elements:");
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
---
+++ Assignment/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace Assignment
+{
+    public class Solution
+    {
+        public IList<int> MaxFrequency(int[] numsArr, int k)
+        {
+            // Array Initialization
+            List<int>[] finalList = new List<int>[numsArr.Length + 1];
+
+            // Dictionary Initialization
+            Dictionary<int, int> countMap = new();
+
+            // Filling the dictionary with unique keys and values
+            foreach (int n in numsArr)
+            {
+
+                if (countMap.ContainsKey(n))
+                {
+                    countMap[n]++;
+                }
+                else
+                {
+                    countMap[n] = 1;
+                }
+            }
+
+            // Passing the dictionary in final list
+            foreach (int key in countMap.Keys)
+            {
+                int frequency = countMap[key];
+                if (finalList[frequency] == null)
+                {
+                    finalList[frequency] = new List<int>();
+                }
+                finalList[frequency].Add(key);
+            }
+
+            List<int> res = new();
+
+            // Variable initialization to keep track of the number of elements added to final list array
+            for (int pos = finalList.Length - 1; pos >= 0 && res.Count < k; pos--)
+            {
+                if (finalList[pos] != null)
+                {
+                    res.AddRange(finalList[pos]);
+                }
+            }
+            return res;
+        }
+    }
+}
